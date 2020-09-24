8b836a59d7dee0f9c5eac096f362adc0125b953a
Gieester Thai <gieestert@student.unimelb.edu.au>
2020-09-24 18:49:34 +1000

    Changed image of 404 page and home button now takes user to profile page if logged in

--- src/notFound.js
+++ src/notFound.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import status404 from './status404.png'
+import { useSelector } from 'react-redux';
+import error404 from './images/error404Image.png';
 // MUI
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
-import ListItem  from '@material-ui/core/ListItem';
+import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,45 +21,57 @@ const useStyles = makeStyles((theme) => ({
   },
   image: {
     margin: 'auto',
+    width: '100%',
   },
   typo: {
-    align: 'center'
+    textAlign: 'center',
   },
   button: {
     backgroundColor: '#192231',
     color: '#FFFFFF',
-  }
+  },
 }));
 
 const NotFound = () => {
+  const [returnRoute, setReturnRoute] = useState('/');
+  const user = useSelector((store) => store.user);
+
+  useEffect(() => {
+    if (user.isAuth) {
+      setReturnRoute(`/${user.id}`);
+    }
+  }, [user]);
+
   const classes = useStyles();
   return (
     <Container maxWidth='md'>
       <Grid className={classes.root} container spacing={3}>
-        <Grid item xs={12} align="center">
-          <img className={classes.image} alt='404' src={status404}  ></img>
+        <Grid item xs={12} align='center'>
+          <img className={classes.image} alt='404' src={error404}></img>
         </Grid>
-        <Grid item xs={12}> 
+        <Grid item xs={12}>
           <List>
             <ListItem>
-              <Typography className={classes.typo} variant='h2'>PAGE NOT FOUND</Typography>
+              <Typography className={classes.typo} variant='h3'>
+                Error - Page not found
+              </Typography>
             </ListItem>
           </List>
         </Grid>
-        <Grid item xs={12}  align='center'>
+        <Grid item xs={12} align='center'>
           <Button
             className={classes.button}
-            component={Link} 
-            size = 'large'
-            to='/'
-            variant='contained' 
+            component={Link}
+            size='large'
+            to={returnRoute}
+            variant='contained'
           >
-            <Typography variant='h5'>HOME PAGE</Typography>
+            <Typography variant='h5'>Home Page</Typography>
           </Button>
         </Grid>
       </Grid>
     </Container>
   );
-}
+};
 
 export default NotFound;
