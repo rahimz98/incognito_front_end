602617f9c9dbc33e6431ba16239d6330868ca642
Gieester Thai <gieestert@student.unimelb.edu.au>
2020-11-01 13:04:26 +1100

    removed commented code

--- src/__tests__/components/createProject.jsx
+++ src/__tests__/components/createProject.jsx
@@ -11,27 +11,4 @@ describe('Create Project', () => {
     expect(screen.getByText(/project visibility/i)).toBeInTheDocument();
     expect(screen.getByText(/project description/i)).toBeInTheDocument();
   });
-
-  // it('renders input for each step with correct buttons', () => {
-  //   const projectName = screen.getByText(/name of project/i);
-  //   expect(projectName).toBeInTheDocument();
-  //   fireEvent.change(projectName, { target: { value: 'Project John' } });
-
-  //   expect(screen.getByText(/next/i)).toBeInTheDocument();
-  //   fireEvent.click(screen.getByText(/next/i));
-  // });
-
-  // it('renders select for project visibility section with back and next buttons', async () => {
-  //   fireEvent.click(screen.getByText('Next'));
-
-  //   screen.debug(null, 20000);
-  //   fireEvent.click(screen.getByText(/next/i));
-  //   screen.debug(null, 20000);
-  // });
-
-  // it('renders input for project description section with back and submit buttons', () => {
-  //   fireEvent.click(screen.getByText(/next/i));
-  //   fireEvent.click(screen.getByText(/next/i));
-  //   screen.debug(null, 20000);
-  // });
 });
